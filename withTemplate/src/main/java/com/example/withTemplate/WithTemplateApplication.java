5f7937b1ff71027ccc10dbd67f8d667cf18b61e3
davidwkaiser <davidwkaiser@yahoo.com>
2018-08-05 15:44:41 -0500

    re add from initializr

---
+++ withTemplate/src/main/java/com/example/withTemplate/WithTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.example.withTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithTemplateApplication.class, args);
+	}
+}
