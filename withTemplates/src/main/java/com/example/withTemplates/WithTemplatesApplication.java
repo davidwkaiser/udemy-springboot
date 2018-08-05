64e6b7b8158a29076966f02cdf0c164206580fe5
davidwkaiser <davidwkaiser@yahoo.com>
2018-08-05 15:37:08 -0500

    add new SpringBoot app

---
+++ withTemplates/src/main/java/com/example/withTemplates/WithTemplatesApplication.java
@@ -0,0 +1,12 @@
+package com.example.withTemplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithTemplatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithTemplatesApplication.class, args);
+	}
+}
