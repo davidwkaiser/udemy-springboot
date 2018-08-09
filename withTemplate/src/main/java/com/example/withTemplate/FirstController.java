2ab563dfa8aa78c3194dadf6358b164de01ec3e8
davidwkaiser <davidwkaiser@yahoo.com>
2018-08-08 21:40:23 -0500

    add templates and exception handlers

--- withTemplate/src/main/java/com/example/withTemplate/FirstController.java
+++ withTemplate/src/main/java/com/example/withTemplate/FirstController.java
@@ -3,7 +3,11 @@ package com.example.withTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
@@ -17,6 +21,23 @@ public class FirstController {
         model.addAttribute("message", getMessage());
         return "first";
     }
+
+    @RequestMapping("/second/{slug}")
+    public String getSlug(@PathVariable(value="slug") String slug, Model model) throws Exception {
+        System.out.println(slug);
+        System.out.println(!slug.equals("dave"));
+        if (!slug.equals("dave")) throw new Exception("We couldn't find a post with "+ slug);
+
+        model.addAttribute("message", getMessage());
+        return "second";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(HttpServletRequest request, Exception exception, Model model){
+        return "error";
+    }
+
+
 
     String getMessage(){
         return firstService.returnMessage();
