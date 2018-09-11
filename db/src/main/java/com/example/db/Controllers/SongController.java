b9be5d49dec35283ee381f43be537d0e2eda43e2
davidwkaiser <davidwkaiser@yahoo.com>
2018-09-10 22:45:07 -0500

    almost works

--- db/src/main/java/com/example/db/Controllers/SongController.java
+++ db/src/main/java/com/example/db/Controllers/SongController.java
@@ -4,23 +4,37 @@ package com.example.db.Controllers;
 import com.example.db.Service.SongService;
 import com.example.db.domain.Song;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class SongController {
 
     @Autowired
     SongService songService;
 
-    @RequestMapping("/songs")
-    public Iterable<Song> returnSongs(){
+//    @RequestMapping("/songs")
+//    public Iterable<Song> returnSongs(){
+//
+//        Iterable<Song> songs = songService.findAll();
+//        System.out.print("CONTROLLER!!!!!!");
+//        for (Song song : songs) {
+//            System.out.print(song);
+//        }
+//        return songs;
+//    }
 
+    @RequestMapping("/songs")
+    public String returnSongs(Model model){
         Iterable<Song> songs = songService.findAll();
-        System.out.print("CONTROLLER!!!!!!");
-        for (Song song : songs) {
-            System.out.print(song);
-        }
-        return songs;
+        model.addAttribute("allSongs", songs );
+        return "songs";
     }
+
+
+
+
+
 }
